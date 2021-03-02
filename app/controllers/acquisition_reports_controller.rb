ea58f89bdf5314989caa1d72ec8e4f3a68b81067
tirnavali <tran.ce.co@gmail.com>
2021-03-02 22:12:21 +0300

    Acquisition analytics edited.

--- app/controllers/acquisition_reports_controller.rb
+++ app/controllers/acquisition_reports_controller.rb
@@ -1,4 +1,7 @@
 class AcquisitionReportsController < ApplicationController
+  before_action :set_acquisition_report, only: [:show, :edit, :update, :destroy] 
+  before_action :reset_form_size, except: [:edit, :new]
+
     def new
       #FIXME initial pub type değerler atansın yada formda göster
       @acquisition_report = AcquisitionReport.new
@@ -6,7 +9,8 @@ class AcquisitionReportsController < ApplicationController
     end
 
     def edit
-      @acquisition_report = AcquisitionReport.find(params[:id])     
+      #@acquisition_report = AcquisitionReport.find(params[:id])  
+      build_forms(params[:form_size])   
     end
 
     def create
@@ -22,7 +26,7 @@ class AcquisitionReportsController < ApplicationController
     end
 
     def update
-      @acquisition_report = AcquisitionReport.find(params[:id])
+      #@acquisition_report = AcquisitionReport.find(params[:id])
       if @acquisition_report.update(acquisition_report_params)
         redirect_to @acquisition_report
       else
@@ -31,14 +35,14 @@ class AcquisitionReportsController < ApplicationController
     end
 
     def destroy
-      @acquisition_report = AcquisitionReport.find(params[:id])
+      #@acquisition_report = AcquisitionReport.find(params[:id])
       #@acquisition_report.acquisition_analytics.destroy
       @acquisition_report.destroy
       redirect_to acquisition_reports_path
     end
 
     def show
-        @acquisition_report = AcquisitionReport.find(params[:id])
+        #@acquisition_report = AcquisitionReport.find(params[:id])
     end
     
     def index
@@ -51,5 +55,24 @@ class AcquisitionReportsController < ApplicationController
         params.require(:acquisition_report).permit(:reporter_identity, :job_title, :posted_books, :refactored_items, :note, :date, 
           acquisition_analytics_attributes: [:id, :pub_arrived_as_supply, :pub_arrived_as_gift, :pub_type_id, :_destroy],
           pub_type_attributes: [:id, :pub_type_name])
+      end
+
+      def set_acquisition_report
+        @acquisition_report = AcquisitionReport.find(params[:id])
+      end
+
+      def build_forms(size)
+        size = size.to_i
+        if size && session[:analytic_form_count].to_i > 0
+          session[:analytic_form_count] += 1
+          session[:analytic_form_count].times { @acquisition_report.acquisition_analytics.build }
+        else size == 1
+          session[:analytic_form_count] = 1
+          @acquisition_report.acquisition_analytics.build
+        end
+      end
+  
+      def reset_form_size
+        session.delete(:analytic_form_count)
       end
 end
