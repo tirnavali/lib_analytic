e140d5d5c9d09142cdc0385e4c7c66e055f7e1db
= <tran.ce.co@gmail.com>
2021-03-20 11:13:30 +0300

    vanilla-nested forms gem added.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("vanilla-nested")
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -10,7 +10,7 @@ require("channels")
 require("jquery")
 require("popper.js")
 
-
+import 'vanilla-nested'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
