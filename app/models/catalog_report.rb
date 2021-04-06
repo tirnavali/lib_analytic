20ef760fbfed30ba803a55875007b6354b07647d
ss90 <tran.ce.co@gmail.com>
2021-04-06 17:57:47 +0300

    catalog report delete function added.

--- app/models/catalog_report.rb
+++ app/models/catalog_report.rb
@@ -1,5 +1,5 @@
 class CatalogReport < ApplicationRecord
-    has_many :catalog_analytics
+    has_many :catalog_analytics, dependent: :destroy
     accepts_nested_attributes_for :catalog_analytics, allow_destroy: true
     validates_associated :catalog_analytics
 end
