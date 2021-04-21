f9b3dd8a38c19334767e9558624f3b3cc7512ea7
tirnavali <tran.ce.co@gmail.com>
2021-04-21 21:47:52 +0300

    internationalization added.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+  	if params[:name]
+  		@name = params[:name]
+  	else
+  		@name = "Unknown Person"
+  	end
     @referance_analytics_count = ReferenceAnalytic.all.count
     @journal_doc_report_count = JournalDocReport.all.count
     @journal_doc_analytic_count = JournalDocReport.all
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.available_locales = [:en, :tr]
+I18n.default_locale = :tr
