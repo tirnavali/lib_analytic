74029888a570e1337376980f86f58d94aa7739cd
ss90 <tran.ce.co@gmail.com>
2021-06-30 21:30:47 +0300

    Users modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:manager, :referancer, :j_documentalist, :acquisitioner], multiple: false)                                      ##
+  petergate(roles: [
+    :system_manager,
+    :manager, 
+    :referancer, 
+    :j_documentalist, 
+    :acquisitioner], multiple: false)                                      ##
   ############################################################################################ 
  
 
