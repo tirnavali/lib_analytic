b669ae2701964870621b345fa149297f67f526c2
ss90 <tran.ce.co@gmail.com>
2021-07-06 12:17:46 +0300

    Some changes applied

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController  
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  after_action :say, only: [:new]
+  before_action :say, only: [:new]
   
   
   # GET /resource/sign_up
