2015965ed3b6cd2481e029575a0cc1fea4c550bb
ss90 <tran.ce.co@gmail.com>
2021-08-13 10:11:31 +0300

    Personel scope was added

--- app/models/personel.rb
+++ app/models/personel.rb
@@ -5,6 +5,12 @@ class Personel < ApplicationRecord
   validates :name, :surname, presence: true
   #accepts_nested_attributes_for :departments
   accepts_nested_attributes_for :employments, allow_destroy: true
+
+  scope :join_department, -> {joins(employments: :department)}
+  scope :department_name, ->(name) {join_department.where("department.name = ?", name)}
+  scope :nil_employment, ->{ includes(:departments).where(employments: {end_date: nil}) }
+  scope :current_department, -> (dep_name) {nil_employment.where("departments.name = ?",dep_name)}
+
     
   def name_with_initial
     "#{name} - #{surname}"
@@ -13,4 +19,11 @@ class Personel < ApplicationRecord
   def to_s
     "#{name} - #{surname}"
   end
+
+  def self.get_by_department(department_name)
+    self.joins(employments: :department)
+    # self.employments.order(:start_date).last
+    # .department.name
+  end
+
 end
