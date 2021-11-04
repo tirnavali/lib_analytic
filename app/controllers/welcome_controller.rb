11b3efcc5eb498c273fb61445e23cbd71ca2d50c
ss90 <tran.ce.co@gmail.com>
2021-11-04 10:27:28 +0300

    Turkish translates added. Some index pages redesigned

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-  	if @user
-  		@name = @user.email
-  	else
-  		@name = "Unknown Person"
-  	end
+  	
     @referance_analytics_count = ReferenceAnalytic.all.count
     @journal_doc_report_count = JournalDocReport.all.count
-    @journal_doc_analytic_count = JournalDocReport.all
+    @journal_doc_analytic_count = JournalDocReport.all.count
    end
 
 end
